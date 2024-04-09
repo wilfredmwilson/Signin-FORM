b88d6c457ca34a64bb705e1bb946f04faeb836bd
Wilfred M. Wilson <wilfred.wilson@cloudium.io>
2024-04-09 17:26:25 +0530

    SIGNUP FORM

---
+++ script.js
@@ -0,0 +1,11 @@
+function checkPassword(form) { 
+    password1 = form.password1.value; 
+    password2 = form.password2.value; 
+
+  
+     if (password1 != password2) { 
+        alert ("\nPassword did not match: Please try again...") 
+        return false; 
+    } 
+
+    } 
